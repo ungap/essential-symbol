1b144e15da3a4385cd2216cdf188023fa6d4a8d7
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-05-02 14:51:11 +0200

    made it dualmodule

--- min.js
+++ min.js
@@ -1,2 +1,2 @@
 /*! (c) Andrea Giammarchi - ISC */
-var self=this||{};try{self.Symbol=Symbol}catch(Symbol){!function(t,n,r){"use strict";function e(n){return new t("Symbol(_@ungap/"+n+")")}e["for"]=function(t){return n[t]||(n[t]=e(r++))},self.Symbol=e}(String,Object.create(null),Math.random())}
+var self=this||{};try{self.Symbol=Symbol}catch(t){!function(n,r,e){"use strict";function l(t){return new n("Symbol(_@ungap/"+t+")")}l.for=function(t){return r[t]||(r[t]=l(e++))},self.Symbol=l}(String,Object.create(null),Math.random())}
